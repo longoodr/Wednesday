23fc7360b8aec4f90f9c4676ec566b5e09530581
Ryan Longood <longoodr@gmail.com>
2021-01-12 21:11:34 -0800

    record (t, (x,y) ) for each move

--- src/scribble.py
+++ src/scribble.py
@@ -1,13 +1,20 @@
 from pynput import mouse
+import time
 
 # Analyze the scribbles from the user between 2 clicks.
+
+datapoints = []
+start_time = time.time()
 
 def on_click(x, y, button, pressed):
     print("Stopping")
     return False # terminates listener
 
 def on_move(x, y):
-    print(f"move  {x} {y}")
+    datapoints.append(
+        (time.time() - start_time, 
+        (x, y)))
+    print(datapoints)
 
 if (__name__ == "__main__"):
     print("Click to begin")
