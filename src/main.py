c5fb52afe3bcd5a69dfcdf3c45444c1aa63bd2f3
Ryan Longood <longoodr@gmail.com>
2021-01-13 22:13:19 -0800

    work on reading scribbles

--- src/main.py
+++ src/main.py
@@ -1,30 +1,53 @@
 import random
 
 from PIL import Image, ImageDraw
-from os import path, walk
+from os import listdir, path, walk
 
-from scribble import get_filename
+import util
+
+from scribble import DIR as SCRIBBLE_DIR, get_filename as get_scribble_filename
 
 SCRIBBLE_WIDTH = 5
-
 
 def run_processing_pipeline(img, pipeline):
     for operation in pipeline:
         operation(img)
 
 def get_num_scribbles():
-    return len([f for f in listdir("scribbles") if path.isfile(path.join("scribbles", f))])
+    num_scribbles = len([f for f in listdir(SCRIBBLE_DIR) if path.isfile(path.join(SCRIBBLE_DIR, f))])
+    if num_scribbles == 0:
+        raise FileNotFoundError
+    return num_scribbles
 
 def read_scribble_data(scribble_filename):
     with open(scribble_filename, "r") as scribble_file:
         return json.load(scribble_file)
 
+def read_scribble_dims():
+    with open("res/img_scribble_dimensions.txt", "r") as dimfile:
+        lines = [line for line in dimfile.readlines() if not line.startswith("#")]
+        dimline = lines[0]
+        dimlist = []
+        for dim in dimline.split():
+            dimlist.append(int(dim))
+        return tuple(dimlist)
+
+def scale_scribble_to_img(scribble_data, img):
+    scribble_dims = read_scribble_dims()
+    return [util.norm_to_pixel_space(util.get_scaled_pt(pt, 1.0, *scribble_dims)) 
+        for pt in scribble_data]
+
 def scribble_from_filename(img, scribble_filename):
     scribble_data = read_scribble_data(scribble_filename)
-    pass
+    scaled_scribble = scale_scribble_to_img(scribble_data)
+
+def draw_scribble_on_img(img, pts):
+    cur_pt = pts[0]
+    draw = ImageDraw.Draw(img)
+    draw.line(pts, fill=255, width=SCRIBBLE_WIDTH, joint="curve")
 
 def scribble(img):
-    rand_scribble_filename = get_filename(random.randrange(get_num_scribbles()))
+    rand_scribble_filename = get_scribble_filename(random.randrange(get_num_scribbles()))
     scribble_from_filename(img, scribble_filename)
 
 def write_text(img):
@@ -33,7 +56,7 @@ def write_text(img):
 pipeline = [scribble, write_text]
 
 if (__name__ == "__main__"):
-    with Image.open("res/img.jpg") as img:
+    with Image.open(path.abspath(path.join("..", "res", "img.jpg"))) as img:
         run_processing_pipeline(img, pipeline)
         img.show()
 
--- src/scribble.py
+++ src/scribble.py
@@ -4,25 +4,27 @@ import json
 from os import mkdir, path
 from pynput import mouse
 
-from util import get_min_max_tuple, get_scaled_num, get_scaled_pt
+import util
 
 # Record the scribbles from the user between 2 clicks and generates
 # a list of (t, (x, y)) coordinates where each coordinate is scaled
 # to lie within [0, 1], where 0 corresponds to the minimum seen raw coord
 # and 1 to the max. Outputs a new data file to scribbles.
 
+DIR = path.join("..", "scribbles")
+
 recording = False
 input_datapoints = []
 start_time = None
 
-def process_data(input_datapoints):
+def normalize_data(input_datapoints):
     output_datapoints = []
     max_t = input_datapoints[-1][0]
     coords = [(x, y) for (_, (x, y)) in input_datapoints]
-    min_x, max_x = get_min_max_tuple([x for (x, _) in coords])
-    min_y, max_y = get_min_max_tuple([y for (_, y) in coords])
+    min_x, max_x = util.get_min_max_tuple([x for (x, _) in coords])
+    min_y, max_y = util.get_min_max_tuple([y for (_, y) in coords])
     for pt in input_datapoints:
-        output_datapoints.append(get_scaled_pt(pt, max_t, min_x, max_x, min_y, max_y))
+        output_datapoints.append(util.get_scaled_pt(pt, max_t, min_x, max_x, min_y, max_y))
     return output_datapoints
 
 # listener
@@ -38,7 +40,7 @@ def on_move(x, y):
     input_datapoints.append((t, (x, y)))
 
 def get_filename(fno):
-    return f"scribbles/scribble{fno}.json"
+    return path.abspath(path.join(DIR, f"scribble{fno}.json"))
 
 def on_click(x, y, button, pressed):
     global recording
@@ -50,14 +52,15 @@ def on_click(x, y, button, pressed):
         return
     print("Stopping")
     recording = False
-    processed = process_data(input_datapoints)
-    if not path.isdir("scribbles"):
-        mkdir("scribbles")
+    processed = normalize_data(input_datapoints)
+    if not path.isdir(DIR):
+        mkdir(DIR)
     
     fno = 0
     while path.isfile(get_filename(fno)):
         fno += 1
     with open(get_filename(fno), "w+") as out_file:
+        print(get_filename(fno))
         json.dump(processed, out_file, indent=1)
     return False # terminates listener
 
